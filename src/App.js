1280454a8ae5348a93c9ffb0b2c6f14277728212
khushaboopandey <pandeykhushaboo@gmail.com>
2023-06-28 13:38:23 +0530

    addes search functanility

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monster: [],
+      searchField: "",
+    };
+    console.log("1");
+  }
+  componentDidMount() {
+    console.log("3");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) =>
+        this.setState(() => {
+          return { monster: json };
+        })
+      );
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
+  render() {
+    const { monster, searchField } = this.state;
+    const { onSearchChange } = this;
+    console.log("2");
+    const filteredData = monster.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+    return (
+      <div className="App">
+        <input
+          className="serach-box"
+          type="search"
+          placeholder="search monster"
+          onChange={onSearchChange}
+        />
+        {filteredData.map((data) => {
+          return <h1 key={data.id}>{data.name}</h1>;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
