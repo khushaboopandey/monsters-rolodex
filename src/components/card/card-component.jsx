acabe0d7465b64116b563f7c527cb9f567419932
khushaboopandey <pandeykhushaboo@gmail.com>
2023-06-29 18:39:00 +0530

    migrating card to functional component.

--- src/components/card/card-component.jsx
+++ src/components/card/card-component.jsx
@@ -1,19 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import "./card-component.styles.css";
 
-export default class Card extends Component {
-  render() {
-    return (
-      <div>
-        <div className="card-container" key={this.props.data.id}>
-          <img
-            src={`https://robohash.org/${this.props.data.id}/?set=set2`}
-            alt={`monster ${this.props.data.name}`}
-          />
-          <h2>{this.props.data.name}</h2>
-          <p>{this.props.data.email}</p>
-        </div>
+export default function Card(props) {
+  return (
+    <div>
+      <div className="card-container" key={props.data.id}>
+        <img
+          src={`https://robohash.org/${props.data.id}/?set=set2`}
+          alt={`monster ${props.data.name}`}
+        />
+        <h2> {props.data.name}</h2>
+        <p>{props.data.email}</p>
       </div>
-    );
-  }
+    </div>
+  );
 }
